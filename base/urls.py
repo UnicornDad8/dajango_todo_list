2ef7f3e588ed8a2e5a73a9bdbf42a6f2133751da
UnicornDad8 <unicorndad8@gmail.com>
2023-05-29 17:55:29 -0400

    Added form

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
-    path('', views.task_list, name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
